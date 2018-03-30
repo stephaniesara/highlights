9110967e644accbc9ef61533f13acf0d7666610f
stephanie chong <stephaniesarachong@gmail.com>
2018-03-30 05:07:54 +0000

    Change port to 80

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./app.js');
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => {
   console.log(`Server is currently running on port ${port}!`);
