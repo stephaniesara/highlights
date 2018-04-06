c0b662d80275f30c84291dfd052cb7ab4ae8837f
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-04-05 19:13:23 -0700

    Include weighted artillery tests

--- load-testing/testFunctions.js
+++ load-testing/testFunctions.js
@@ -1,11 +1,17 @@
 'use strict';
 
 module.exports = {
-	getRandomBusinessIterator
+	getWeightedBusinessIterator
 };
 
 function getRandomBusinessIterator(userContext, events, done) {
 	const randIterator = Math.floor(Math.random() * 10000000);
+	userContext.vars.iterator = randIterator;
+	return done();
+}
+
+function getWeightedBusinessIterator(userContext, events, done) {
+	const randIterator = Math.floor(Math.random() * 10000);
 	userContext.vars.iterator = randIterator;
 	return done();
 }
