5458efaf2da06d023835ac7c5a22651f45918297
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-05-13 14:28:49 -0700

    Cleanup server files

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ import indexHtml from "./indexHtml.js";
 import html from "./html.js";
 
 // serve static files
-//app.use('/bundle.js', express.static('./dist/bundle.js'));
 app.use(express.static("./dist"));
 
 // returns html template of Highlights only - for proxy server
@@ -56,17 +55,5 @@ app.get("/main/highlights/ssr/:iterator", (req, res) => {
     }
   });
 });
-
-// returns a complete index.html template
-// app.get('/main/highlights/ssr/:iterator', (req, res) => {
-//   var iterator = req.params.iterator;
-//   var query = `select sentence, keyword, count, photo_url, is_business_photo from highlight where iterator = ${iterator} order by count desc`;
-//   dbCassandra.execute(query, (err, result) => {
-//     if (err) throw err;
-//     const props = { highlights: result.rows };
-//     const body = renderToString(React.createElement(Highlights, props));
-//     res.send(indexHtml(body, JSON.stringify(props)))
-//   })
-// });
 
 module.exports = app;
