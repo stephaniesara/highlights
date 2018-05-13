f0398af7273e9eca781ea6d2d56502ae20be30d8
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-05-13 14:29:31 -0700

    Fix syntax error

--- load-testing/testFunctions.js
+++ load-testing/testFunctions.js
@@ -1,16 +1,17 @@
-'use strict';
+"use strict";
 
 module.exports = {
-	getWeightedBusinessIterator
+  getWeightedBusinessIterator
 };
 
 function getRandomBusinessIterator(userContext, events, done) {
-	const randIterator = Math.floor(Math.random() * 10000000);
-	userContext.vars.iterator = randIterator;
-	return done();
+  const randIterator = Math.floor(Math.random() * 10000000);
+  userContext.vars.iterator = randIterator;
+  return done();
 }
 
 function getWeightedBusinessIterator(userContext, events, done) {
-	const randIterator = Math.floor(Math.random() * 11000);
-	userContext.vars.iterator = randIterator;
-	return done();
+  const randIterator = Math.floor(Math.random() * 11000);
+  userContext.vars.iterator = randIterator;
+  return done();
+}
